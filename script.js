a3f489d7fdf831cafe08fd0e897be683574badf5
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-12-15 23:13:11 -0500

    added text input field and displays on screen

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let searchInput = document.querySelector("#search-text");
 let replaceInput = document.querySelector("#replace-text");
-let buttonEl = document.querySelector("#enter-button")
+let textInput = document.querySelector("#input-text");
+let textField = document.querySelector("#text-field");
+let searchButtonEl = document.querySelector("#search-button");
+let textButtonEl = document.querySelector("#text-button")
 
-let text = "";
+// let textField = "";
 
-buttonEl.addEventListener("click", function(event){
+searchButtonEl.addEventListener("click", function(event){
     event.preventDefault();
     console.log("clicked!");
 
@@ -14,4 +17,42 @@ buttonEl.addEventListener("click", function(event){
     console.log(searchText);
     console.log(replaceText);
 
-})
+});
+
+textButtonEl.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("clicked!");
+
+    let searchText = textInput.value.trim();
+    // textField.push.
+    console.log(searchText);
+
+    textField.textContent = searchText
+
+});
+
+// function renderText() {
+//     // textField.innerHTML = "";
+//     textField.textContent
+// }
+
+// function renderTodos() {
+//   // Clear todoList element and update todoCountSpan
+//   todoList.innerHTML = "";
+//   todoCountSpan.textContent = todos.length;
+
+//   // Render a new li for each todo
+//   for (var i = 0; i < todos.length; i++) {
+//     var todo = todos[i];
+
+//     var li = document.createElement("li");
+//     li.textContent = todo;
+//     li.setAttribute("data-index", i);
+
+//     var button = document.createElement("button");
+//     button.textContent = "Complete";
+
+//     li.appendChild(button);
+//     todoList.appendChild(li);
+//   }
+// }
