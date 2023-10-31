b9dd39a36e19e053d3f09f5270117a0da9a57c3b
Carter Neal <55357370+nealwc@users.noreply.github.com>
2023-10-31 14:26:30 -0400

    added clear button and logic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let replace = document.querySelector("#replace");
 let textInput = document.querySelector("#input-text");
 let textField = document.querySelector("#text-field");
 let searchButton = document.querySelector("#search-button");
+let clearButton = document.querySelector("#clear-button");
 let textButton = document.querySelector("#text-button")
 
 let searchField = "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way. - Michael Scott";
@@ -14,16 +15,20 @@ function renderSearchField() {
     textField.textContent = searchField
 }
 
+// clear input fields
+clearButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    search.value = "";
+    replace.value = "";
+})
+
 // search and replace function when searchButton is clicked
 searchButton.addEventListener("click", function (event) {
     event.preventDefault();
 
     let searchText = search.value.trim().toLowerCase();
     let replaceText = replace.value.trim();
-
-    // clear input fields
-    // search.value = "";
-    // replace.value = "";
 
     if (searchText === replaceText) {
         alert("Search and Replace text matches")
